978f5b51b58a464e72587f2da969ee225bf0f6cd
Richard Hildred <rhildred@gmail.com>
2024-11-04 18:35:08 +0000

    add style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import { Hono } from 'hono'
+import { Hono,  } from 'hono'
 import { renderer } from './renderer'
+import { serveStatic } from "@hono/node-server/serve-static"
+
 
 const app = new Hono()
 
@@ -8,5 +10,6 @@ app.use(renderer)
 app.get('/', (c) => {
   return c.render(<h1>Hello World!</h1>)
 })
+app.use("/*", serveStatic({ root: "public/"})) // path must end with '/'
 
 export default app
